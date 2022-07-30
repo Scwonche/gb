7ac9c6f58a475ea19696854bd8d78247a307f47e
User <kusanjvzhas@mail.ru>
2022-07-30 10:15:25 +0500

    первая версия

---
+++ main.py
@@ -0,0 +1,9 @@
+from pydnevnikruapi.dnevnik import dnevnik
+
+login = "daniiarmendygaliev"
+password = "lfybzh"
+
+dn = dnevnik.DiaryAPI(login=login, password=password)
+
+
+print(dn.get_final_group_marks_by_subject(group_id=1831168584419618567, subject_id=1192159007994913))
