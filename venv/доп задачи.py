cc7036f575b6225179500d1e289876f30721b460
User <kusanjvzhas@mail.ru>
2022-07-31 14:52:33 +0500

    6 версия

--- venv/доп задачи.py
+++ venv/доп задачи.py
@@ -20,3 +20,32 @@ for i in range(16):
 print(*a)
 for i in range(15,0,-1):
     print(a[i],end=' ')
+#3
+from random import randint
+
+a = []
+s = 0
+for i in range(16):
+    p = randint(1, 100)
+    a.append(p)
+k = a.index(max(a))
+p = a.index(min(a))
+if k < p:
+    print(sum(a[k:p]))
+else:
+    print(sum(a[p:k]))
+print(*a)
+
+
+#4
+from random import randint
+
+a = []
+s = 0
+for i in range(16):
+    p = randint(1, 100)
+    s += p
+    a.append(p)
+
+print(*a)
+print(s/len(a))
