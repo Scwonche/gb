10576a7d543f29b34458fa334dc8c8d353c01862
User <kusanjvzhas@mail.ru>
2022-07-31 14:51:29 +0500

    5 версия

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ login = "daniiarmendygaliev"
 password = ""
 
 dn = dnevnik.DiaryAPI(login=login, password=password)
-
+пуцпцы
 
 print(dn.get_final_group_marks_by_subject(group_id=1831168584419618567, subject_id=1192159007994913))
