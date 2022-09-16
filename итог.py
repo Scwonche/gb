d8dcf05dd7727e8a1755d868e2b7ac03350be306
User <kusanjvzhas@mail.ru>
2022-09-16 20:30:31 +0500

    итоговая кр 2

--- итог.py
+++ итог.py
@@ -1,2 +1,9 @@
 a = []
 b = []
+n = int(input("введите количество эл-тов массива"))
+for i in range(n):
+    s = input(f'введите {i+1}-ый элемент массива')
+    a.append(s)
+for elem in a:
+    if len(elem)<4:
+        b.append(elem)
